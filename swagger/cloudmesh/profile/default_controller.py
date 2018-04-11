1236f6edb9497a345e70024e35836f4acc9717b2
gyDBD <gydbd06@hotmail.com>
2018-04-11 16:59:56 -0400

    Update default_controller.py

--- swagger/cloudmesh/profile/default_controller.py
+++ swagger/cloudmesh/profile/default_controller.py
@@ -1,8 +1,14 @@
 import connexion
 import six
-from profile_controller import *
+#from profile_controller import *
 from swagger_server.models.profile import PROFILE  # noqa: E501
 from swagger_server import util
+from pymongo import MongoClient
+# Connect to the MongoDB, change the connection string per your MongoDB environment
+client = MongoClient(port=27017)
+# Set the db object to point to the business database
+db=client.profiles
+# Showcasing the count() method of find, count the total number of 5 ratings
 
 
 def get_profile_by_uuid(uuid):
@@ -26,3 +32,13 @@ def profiles_get():  # noqa: E501
     for each in items:
         listOfProfile.append(PROFILE(item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7]))
     return listOfProfile
+
+def get_profile():
+	l = list()
+	for each in db.Profile.find():
+		l.append((each['uuid'], each['username'], each['context'],each['description'],each['firstname'],each['lastname'],each['publickey'],each['email']))
+	return l
+
+def get_profile_by_uuid_mongo(uuid):
+	for each in db.Profile.find({'uuid':uuid}):
+		return (each['uuid'], each['username'], each['context'],each['description'],each['firstname'],each['lastname'],each['publickey'],each['email'])
