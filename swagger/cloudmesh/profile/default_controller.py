bbae3d53f77a2b0cc86fbb95ad82588deab1973f
gyDBD <gydbd06@hotmail.com>
2018-04-11 22:02:45 -0400

    add profile service

--- swagger/cloudmesh/profile/default_controller.py
+++ swagger/cloudmesh/profile/default_controller.py
@@ -30,7 +30,7 @@ def profiles_get():  # noqa: E501
     listOfProfile = []
     items = get_profile()
     for each in items:
-        listOfProfile.append(PROFILE(item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7]))
+        listOfProfile.append(PROFILE(each[0],each[1],each[2],each[3],each[4],each[5],each[6],each[7]))
     return listOfProfile
 
 def get_profile():
@@ -42,3 +42,7 @@ def get_profile():
 def get_profile_by_uuid_mongo(uuid):
 	for each in db.Profile.find({'uuid':uuid}):
 		return (each['uuid'], each['username'], each['context'],each['description'],each['firstname'],each['lastname'],each['publickey'],each['email'])
+
+def add_profile_mongo(uuid, username,context,description,firstname, lastname,publickey, email):
+	db.Profile.insert({"uuid":uuid, "username":username,"context":context, "description":description, "firstname":firstname, "lastname":lastname, "publickey":publickey,"email":email})
+	return "add a new profile successfully"
